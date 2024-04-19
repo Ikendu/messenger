bab7e0cfddeaf2d04f1df83c9ab84cd9637f138c
Aniede Chibundu <ndubest56@gmail.com>
2024-04-19 03:23:59 +0100

    tailwind config

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,4 +1,9 @@
-function ChatBox() {
+import {Avatar, Image} from "antd"
+
+export default function ChatBoxReciever() {
+  return <div><Avatar/></div>;
+}
+
+export default function ChatBoxSender() {
   return <div>ChatBox</div>;
 }
-export default ChatBox;
