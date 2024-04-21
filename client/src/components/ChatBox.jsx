f6fc0cfcbd57af165a4171d244dba0eccc88886a
Aniede Chibundu <ndubest56@gmail.com>
2024-04-21 07:19:26 +0100

    new updates

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,9 +1,22 @@
-import {Avatar, Image} from "antd"
+import { Avatar, Image } from "antd";
 
-export default function ChatBoxReciever() {
-  return <div><Avatar/></div>;
+export default function ChatBoxReciever({ avatar }) {
+  return (
+    <div>
+      <Avatar
+        size={50}
+        src={
+          <Image
+            src={avatar}
+            className="w-11 h-11 rounded-full object-cover bg-slate-500"
+            // preview={false}
+          />
+        }
+      />
+    </div>
+  );
 }
 
-export default function ChatBoxSender() {
+export function ChatBoxSender() {
   return <div>ChatBox</div>;
 }
