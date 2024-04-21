b1f68f55e6b233ee1e25fa65ad16a62563587406
Aniede Chibundu <ndubest56@gmail.com>
2024-04-21 17:33:20 +0100

    user and sender chats display

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import socketIOClient from "socket.io-client";
 import UserLogin from "./UserLogin";
+import { ChatBoxReciever, ChatBoxSender } from "./ChatBox";
 
 function ChatContainer() {
   const socketio = socketIOClient(`http://localhost:5000`);
@@ -30,6 +31,24 @@ function ChatContainer() {
     localStorage.removeItem(`avater`);
     setUser(``);
     // window.location.reload();
+  }
+
+  function ChatsList() {
+    return chats.map((chat, index) => {
+      chat.user === user ? (
+        <ChatBoxSender
+          key={index}
+          message={chat.message}
+          avatar={chat.avatar}
+        />
+      ) : (
+        <ChatBoxReciever
+          key={index}
+          message={chat.message}
+          avatar={chat.avatar}
+        />
+      );
+    });
   }
 
   return (
