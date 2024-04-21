41e99c83f2ac7fe00b208a7a489d165c4ec0e06c
Aniede Chibundu <ndubest56@gmail.com>
2024-04-21 09:08:57 +0100

    chat reciever and sender

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
-import "./App.css";
-import ChatBoxReciever from "./components/ChatBox";
+import { ChatBoxReciever, ChatBoxSender } from "./components/ChatBox";
 
 function App() {
   return (
-    <>
-      <ChatBoxReciever />
-    </>
+    <div className=" bg-slate-300 h-screen">
+      <h1 className="font-bold text-2xl">Life Chat</h1>
+      <ChatBoxReciever
+        avatar={`https://picsum.photos/200/300`}
+        message={`hello my brother`}
+        user={`Younglife`}
+      />
+      <ChatBoxSender
+        avatar={`https://picsum.photos/id/237/200/300`}
+        message={`hello my dear`}
+        user={`Victor`}
+      />
+    </div>
   );
 }
 
--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,22 +1,43 @@
-import { Avatar, Image } from "antd";
+import { Avatar, Image, message } from "antd";
 
-export default function ChatBoxReciever({ avatar }) {
+export function ChatBoxReciever({ avatar, user, message }) {
   return (
-    <div>
+    <div className="flex gap-2 justify-start mx-2">
       <Avatar
         size={50}
         src={
           <Image
             src={avatar}
             className="w-11 h-11 rounded-full object-cover bg-slate-500"
-            // preview={false}
+            preview={false}
           />
         }
       />
+      <div className="bg-blue-300 p-2 rounded-md max-w-[60%]  ">
+        <p className="font-bold">{user}</p>
+        {message}
+      </div>
     </div>
   );
 }
 
-export function ChatBoxSender() {
-  return <div>ChatBox</div>;
+export function ChatBoxSender({ avatar, user, message }) {
+  return (
+    <div className="flex gap-2 flex-row-reverse  mx-2">
+      <Avatar
+        size={50}
+        src={
+          <Image
+            src={avatar}
+            className="w-11 h-11 rounded-full object-cover"
+            preview={false}
+          />
+        }
+      />
+      <div className="bg-green-300 p-2 rounded-md max-w-[60%] ">
+        <p className="font-bold">{user}</p>
+        {message}
+      </div>
+    </div>
+  );
 }
