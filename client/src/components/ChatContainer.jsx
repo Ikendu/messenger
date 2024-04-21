1c82551a8cc3a987c9ab45fdc4b9988fce2d36ed
Aniede Chibundu <ndubest56@gmail.com>
2024-04-21 11:25:29 +0100

    chat contents

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+import socketIOClient from "socket.io-client";
+
 function ChatContainer() {
-  return <div>ChatContainer</div>;
+  const socketio = socketIOClient(`https://localhost:5000`);
+
+  const [caht, setChat] = useState([]);
+  const [user, setUser] = useState(localStorage.getItem(`user`));
+  const [avatar, setAvatar] = useState(localStorage.getItem(`avatar`));
+
+  return <div>hello</div>;
 }
 export default ChatContainer;
--- client/src/components/UserLogin.jsx
+++ client/src/components/UserLogin.jsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
 import ChatIcon from "../assets/ChatIcon";
+import _ from "lodash";
 
-function UserLogin() {
-  const [user, setUser] = useState(``);
+function UserLogin({ setUser }) {
+  const [user, setAUser] = useState(``);
+
+  function handleUserLogin() {
+    setUser(user);
+    localStorage.setItem(`user`, user);
+    localStorage.setItem(
+      `avatar`,
+      `https://picsum.photos/id/${_.random(1, 1000)}/200/300`
+    );
+  }
   return (
     <div className="h-screen flex flex-col gap-20 justify-center items-center">
       <div className="flex gap-3 items-center">
@@ -14,9 +24,12 @@ function UserLogin() {
           className="border-2 border-green-500 w-full rounded-md px-4 p-1"
           placeholder="enter username"
           value={user}
-          onChange={(e) => setUser(e.target.value)}
+          onChange={(e) => setAUser(e.target.value)}
         />
-        <button className="text-lg font-bold text-white px-6 py-3 bg-green-500 rounded-md">
+        <button
+          onClick={handleUserLogin}
+          className="text-lg font-bold text-white px-6 py-3 bg-green-500 rounded-md"
+        >
           Enter
         </button>
       </div>
